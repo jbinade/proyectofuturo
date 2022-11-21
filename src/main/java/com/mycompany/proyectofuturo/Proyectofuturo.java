7cef5d3120772a0c42bdab72ff66e3dab17de15c
Jesus <94246161+jbinade@users.noreply.github.com>
2022-11-21 18:30:05 +0100

    version inicial

---
+++ src/main/java/com/mycompany/proyectofuturo/Proyectofuturo.java
@@ -0,0 +1,28 @@
+
+package com.mycompany.proyectofuturo;
+
+
+public class Proyectofuturo {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Estudiante: " + args[0]);
+      
+        System.out.println(veredicto(args[2], args[1]));
+    }
+
+
+    public static String veredicto(String valor1, String valor2) {
+       
+    if (valor1.equals(valor2)) {
+    return "Bien, pero necesitas esforzarte algo más.";
+    }
+    if (Integer.valueOf(valor1) < Integer.valueOf(valor2)){
+    return "Poco tiempo de estudio. Necesitar dedicar más tiempo.";
+    }
+    return "Genial. Trabajas bien en casa.";
+        
+       
+    }
+    
+}    
